816351a1f5453ceabdde8bc2dc20393a953e0c4b
kanno shunsuke <syunsuke.05.23.15@gmail.com>
2024-07-05 19:38:31 +0900

    edit:Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,7 @@ use Throwable;
 use Illuminate\Session\TokenMismatchException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -33,8 +34,21 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        // 419エラー（TokenMismatchException）のハンドリング
-        //dd($exception);
+        make_error_log("server_error.log", "=================start================");
+        
+        $log_message = [
+            'message' => $exception->getMessage(),
+            'exception' => get_class($exception),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => $exception->getTraceAsString(),
+            'url' => request()->fullUrl(),
+            'method' => request()->method(),
+            'input' => request()->all(),
+        ];
+        make_error_log("server_error.log", print_r($log_message,1));
+
+        // 419エラー（TokenMismatchException）トークンが無効
         if ($exception instanceof TokenMismatchException) {
             $message = ['message' => 'セッションの有効期限が切れました。もう一度ログインしてください。',
                         'type' => 'error',
@@ -44,6 +58,7 @@ class Handler extends ExceptionHandler
 
         //postメソッドをgetで呼び出した場合
         if ($exception instanceof MethodNotAllowedHttpException) {
+            make_error_log("server_error.log", "Method does not match  =" . print_r($log_message,1));
             $message = ['message' => 'このページはPOSTメソッドのみサポートしています。', 
                         'type' => 'error',
                         'sec' => '2000'];
@@ -52,11 +67,25 @@ class Handler extends ExceptionHandler
         
         //404エラー　対処のページがない
         if ($exception instanceof NotFoundHttpException) {
+            make_error_log("server_error.log", "[404]error not_page  =" . print_r($log_message,1));
             $message = ['message' => 'ページが見つかりませんでした。',
                         'type' => 'error',
                         'sec' => '2000'];
             return redirect()->route('home')->with($message);
         }
+
+        // 500エラー（Internal Server Error）のハンドリング
+        if ($exception instanceof HttpException && $exception->getStatusCode() === 500) {
+            make_error_log("server_error.log", "[500]error  =" . print_r($log_message,1));
+            $message = [
+                        'message' => '内部サーバーエラーが発生しました。',
+                        'type' => 'error',
+                        'sec' => '2000'
+            ];
+            return redirect()->route('home')->with($message);
+        }
+
+
         return parent::render($request, $exception);
     }
 }
