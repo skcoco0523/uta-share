c9e49b7cc5aacee68854196362d4198e5f24aa0c
kanno shunsuke <syunsuke.05.23.15@gmail.com>
2024-07-22 11:24:25 +0900

    edit:nav_menu_rankking_stop

--- resources/views/layouts/nav_menu.blade.php
+++ resources/views/layouts/nav_menu.blade.php
@@ -6,11 +6,13 @@
                 <img src="{{ asset('img/icon/home.png') }}" alt="アイコン" class="icon-top">
                 <span style="font-size: 0.75rem;">トップ</span>
             </a>
+            {{-- ランキング機能は現在ないため無効化
             <div class="border-right"></div>
             <a class="flex-sm-fill text-sm-center nav-link p-2 d-flex flex-column align-items-center" href="#">
                 <img src="{{ asset('img/icon/ranking.png') }}" alt="アイコン" class="icon-top">
                 <span style="font-size: 0.75rem;">ランキング</span>
             </a>
+            --}}
             <div class="border-right"></div>
             <a href="{{ route('search-show') }}" class="flex-sm-fill text-sm-center nav-link p-2 d-flex flex-column align-items-center" href="#">
                 <img src="{{ asset('img/icon/search.png') }}" alt="アイコン" class="icon-top">
